12c8f6ceb6b0c51e45e403464837da14d1868316
mac <mac@Ikems-MacBook-Pro.local>
2023-03-29 16:59:40 +0100

    update performOperation function

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let operator = prompt('select an operator, a. addition, b. subtraction, c. divis
 function checkOperator(){
     if (operator === 'a' || operator === 'b' || operator === 'c' || operator === 'd' && operator != '') {
         performOperation();
-    } else{
+    } else {
         console.log('please insert a valid input of operation!');
     }
     resetOperationSelection();
@@ -16,20 +16,44 @@ function checkOperator(){
 
 checkOperator();
 
-function resetOperatorSelection() {
-    let operator = prompt('select an operator, a. addition, b. subtraction, c. division, d. multiplication: ');
-    checkOperator();
-}
+
+
 //4. create a function that will perform our calculation based on the operator selected by the user
 function performOperatorSelection() {
     //  a. ask user for first number or input
-    let firstInput = prompt('please insert your first number: ');
+    let firstInput = parseInt(prompt('please insert your first number: '));
 
     //  b. ask user second number or input
-    let secondInput = prompt('please insert your second number: ');
+    let secondInput = parseInt(prompt('please insert your second number: '));
 
+//  c. perform the operation selected on the first and second numbers inputed
+
+if (operator === 'a') {
+    let addition = firstInput + secondInput
+    console.log(`the sum is ${addition}`);
+    return addition;
+} else if (operator === 'b') {
+    let subtraction = firstInput - secondInput
+    console.log(`the sum is ${addition}`);
+    return subtraction;
+} else if (division === 'c') {
+    let division = firstInput / secondInput
+    console.log(`the sum is ${division}`);
+    return division;
+} else if (operator === 'd') {
+    let multiplication = firstInput * secondInput
+    console.log(`the sum is ${multiplication}`);
+    return multiplication;
+} else {
+    console.log('please insert a valid number or input!')
 }
+performOperation();
 //  a. ask user for first number or input
 //  b. ask user second number or input
 //  c. perform the operation selected on the first and second numbers inputed
 // create a function to reset the input of operator selection and restart the process when there is a wrong selection of operator
+function resetOperatorSelection() {
+    let operator = prompt('select an operator, a. addition, b. subtraction, c. division, d. multiplication: ');
+    checkOperator();
+}
+}
