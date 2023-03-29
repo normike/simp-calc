91d9bf6fbfa9eb6164ea7a02d5b81cf56c92c7e9
mac <mac@Ikems-MacBook-Pro.local>
2023-03-29 15:38:57 +0100

    create performOperation function

---
+++ script.js
@@ -0,0 +1,35 @@
+//1. define prompt so we can respond from our ide 
+const prompt = require('prompt-sync')();
+
+//2. create a prompt for user to select the type of operation to perform 
+let operator = prompt('select an operator, a. addition, b. subtraction, c. division, d. multiplication: ');
+
+//3. create a function that checks the user selection of operator if the operator selected is correct proceed else ask the user to input a correct selector and start over
+function checkOperator(){
+    if (operator === 'a' || operator === 'b' || operator === 'c' || operator === 'd' && operator != '') {
+        performOperation();
+    } else{
+        console.log('please insert a valid input of operation!');
+    }
+    resetOperationSelection();
+}
+
+checkOperator();
+
+function resetOperatorSelection() {
+    let operator = prompt('select an operator, a. addition, b. subtraction, c. division, d. multiplication: ');
+    checkOperator();
+}
+//4. create a function that will perform our calculation based on the operator selected by the user
+function performOperatorSelection() {
+    //  a. ask user for first number or input
+    let firstInput = prompt('please insert your first number: ');
+
+    //  b. ask user second number or input
+    let secondInput = prompt('please insert your second number: ');
+
+}
+//  a. ask user for first number or input
+//  b. ask user second number or input
+//  c. perform the operation selected on the first and second numbers inputed
+// create a function to reset the input of operator selection and restart the process when there is a wrong selection of operator
